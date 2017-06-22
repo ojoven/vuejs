b942045a8d8f3e3c6f5650698cf68c517880cc1a
ojoven <mikeltorresmail@gmail.com>
2017-06-22 13:14:56 +0200

    Add games.js on Vue Tutorial

---
+++ js/games.js
@@ -0,0 +1,63 @@
+Vue.component('game-add', {
+	template: `
+        <div>
+            <input type="text" v-model="titleGame" />
+            <button @click="emitNewGame">Añadir</button>
+        </div>
+    `,
+	data: function () {
+		return {
+			titleGame: null
+		}
+	},
+	methods: {
+		emitNewGame: function () {
+			if (this.titleGame) {
+				this.$emit('new', { title: this.titleGame });
+				this.titleGame = null;
+			}
+		}
+	},
+});
+
+Vue.component('game-list', {
+	props: ['games'],
+	template: `
+        <ol>
+            <game-item v-for="item in games" :game="item" :key="item.id"></game-item>
+        </ol>
+    `
+});
+
+Vue.component('game-item', {
+	props: ['game'],
+	template: '<li>{{ game.title }}</li>'
+});
+
+Vue.component('game-header', {
+	template: '<h1>Video Games</h1>'
+});
+
+const app = new Vue({
+	el: '#app',
+	template: `
+        <div class="view">
+            <game-header></game-header>
+            <game-add @new="addNewGame"></game-add>
+            <game-list v-bind:games="games"></game-list>
+        </div>
+    `,
+	data: {
+		message: 'Video Games',
+		games: [
+			{ title: 'ME: Andromeda' },
+			{ title: 'Fifa 2017' },
+			{ title: 'League of Legend' }
+		]
+	},
+	methods: {
+		addNewGame: function (game) {
+			this.games.push(game);
+		}
+	}
+});
